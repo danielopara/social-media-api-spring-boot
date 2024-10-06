2d3c7fba2dcb30756642197c2d70d9bb565e5918
Daniel Opara <oparadaniv@gmail.com>
2024-10-06 01:41:45 +0100

    first commit

---
+++ src/test/java/com/danielopara/Social_Media_API/SocialMediaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielopara.Social_Media_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
