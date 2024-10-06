fb2e8aefe9bed136ac161f093ba6126d5f17b0a7
Daniel Opara <oparadaniv@gmail.com>
2024-10-06 19:47:48 +0100

    created CustomAuthenticationEntryPoint

---
+++ src/main/java/com/danielopara/Social_Media_API/utils/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,35 @@
+package com.danielopara.Social_Media_API.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setContentType("application/json");
+        String requestURI = request.getRequestURI();
+
+
+        Map<String, String > responseBody = new HashMap<>();
+        responseBody.put("status", String.valueOf(HttpStatus.FORBIDDEN.value()));
+        responseBody.put("error", "Unauthorized");
+        responseBody.put("message", authException.getMessage());
+        responseBody.put("url", requestURI);
+        responseBody.put("path", request.getServletPath());
+
+
+        String jsonResponse = new ObjectMapper().writeValueAsString(responseBody);
+
+        response.getWriter().write(jsonResponse);
+    }
+}
