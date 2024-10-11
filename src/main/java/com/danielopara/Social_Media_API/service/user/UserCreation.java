842042e9b6e18d7afc67b82574ea88e4598bc68d
Daniel Opara <67028678+danielopara@users.noreply.github.com>
2024-10-11 16:26:46 +0100

    Update UserCreation.java

--- src/main/java/com/danielopara/Social_Media_API/service/user/UserCreation.java
+++ src/main/java/com/danielopara/Social_Media_API/service/user/UserCreation.java
@@ -4,6 +4,7 @@ import com.danielopara.Social_Media_API.dto.CreateUserDto;
 import com.danielopara.Social_Media_API.models.UserModel;
 import com.danielopara.Social_Media_API.response.BaseResponse;
 
+//user creation
 public interface UserCreation {
     BaseResponse createUser(CreateUserDto createUserDto);
 }
