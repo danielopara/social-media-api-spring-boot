210cac7ce2ce6958d3e5993eecdf99c69a225859
Daniel Opara <oparadaniv@gmail.com>
2024-10-12 21:04:47 +0100

    updated account repository

--- src/main/java/com/danielopara/Social_Media_API/Repository/AccountRepository.java
+++ src/main/java/com/danielopara/Social_Media_API/Repository/AccountRepository.java
@@ -3,5 +3,8 @@ package com.danielopara.Social_Media_API.Repository;
 import com.danielopara.Social_Media_API.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findByEmail(String email);
 }
