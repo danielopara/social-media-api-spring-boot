bdaea9f99a5bdeebf9397c5eee9ecefc708836c9
Daniel Opara <oparadaniv@gmail.com>
2024-11-19 11:05:44 +0100

    updating FollowRepository.java

--- src/main/java/com/danielopara/Social_Media_API/Repository/FollowRepository.java
+++ src/main/java/com/danielopara/Social_Media_API/Repository/FollowRepository.java
@@ -1,7 +1,15 @@
 package com.danielopara.Social_Media_API.Repository;
 
+import com.danielopara.Social_Media_API.models.Account;
 import com.danielopara.Social_Media_API.models.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface FollowRepository extends JpaRepository<Follow, Long> {
+    Optional<Follow> findByFollowerAndFollowing(Account follower, Account following);
+    Optional<Follow> findByFollowerId(Long id);
+    boolean existsByFollowerAndFollowing(Account follower, Account following);
 }
