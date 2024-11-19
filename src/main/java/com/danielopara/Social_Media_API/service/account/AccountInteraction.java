385133528285ac6f7d34922e3911f6423464a51e
Daniel Opara <oparadaniv@gmail.com>
2024-11-19 11:08:34 +0100

    updated code

--- src/main/java/com/danielopara/Social_Media_API/service/account/AccountInteraction.java
+++ src/main/java/com/danielopara/Social_Media_API/service/account/AccountInteraction.java
@@ -1,8 +1,13 @@
 package com.danielopara.Social_Media_API.service.account;
 
+import com.danielopara.Social_Media_API.models.Account;
 import com.danielopara.Social_Media_API.response.BaseResponse;
+
+import java.util.List;
 
 public interface AccountInteraction {
     void followAccount(String email, Long accountId);
     BaseResponse unfollowAccount(String email, Long accountId);
+    BaseResponse listFollowingAccountByAccountId(String email);
+    BaseResponse listFollowerAccountByAccountEmail(String email);
 }
