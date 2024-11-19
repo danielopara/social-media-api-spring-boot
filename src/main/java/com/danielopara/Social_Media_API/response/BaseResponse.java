5b1f38dc53a5e610f42000b2a4446c870ce3f55d
Daniel Opara <oparadaniv@gmail.com>
2024-11-19 11:10:52 +0100

    updated code

--- src/main/java/com/danielopara/Social_Media_API/response/BaseResponse.java
+++ src/main/java/com/danielopara/Social_Media_API/response/BaseResponse.java
@@ -1,9 +1,11 @@
 package com.danielopara.Social_Media_API.response;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.context.annotation.Bean;
 
 @Getter
 @Setter
@@ -13,4 +15,13 @@ public class BaseResponse {
     private int statusCode;
     private String message;
     private Object data;
+
+
+    public static BaseResponse createSuccessResponse(String message, Object data ){
+        return new BaseResponse(HttpServletResponse.SC_OK, message, data);
+    }
+
+    public static BaseResponse createErrorResponse(String message, Object data){
+        return new BaseResponse(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, message, data);
+    }
 }
